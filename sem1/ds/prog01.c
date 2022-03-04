09304b19ea04cfda1e442e2ea47d8ab72208395d
Nandu Sasikumar - NCS <nandusasikumar007@gmail.com>
2022-03-05 02:41:40 +0530

    commit 9

---
+++ sem1/ds/prog01.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+	int m,n,a[10],b[10],c[20],i,j,t,k=0;
+	printf("Enter size of array a : ");
+	scanf("%d",&m);
+	printf("Enter array elements : ");
+	for(i=0;i<m;i++) 
+	scanf("%d",&a[i]);
+	printf("Enter size of array b : ");
+	scanf("%d",&n);
+	printf("Enter array elements : ");
+	for(i=0;i<n;i++)
+	scanf("%d",&b[i]);
+	for(i=0;i<m;i++) 
+	for(j=i+1;j<m;j++)
+	if(a[i]>a[j]) {
+		t=a[i];
+		a[i]=a[j];
+		a[j]=t;
+	}
+	for(i=0;i<n;i++) 
+	for(j=i+1;j<n;j++)
+	if(b[i]>b[j]) {
+		t=b[i];
+		b[i]=b[j];
+		b[j]=t;
+	}
+	i=j=0;
+	while(i<m && j<n) {
+		if(a[i]<=b[j]) {
+			c[k]=a[i];
+			i++;
+			k++;
+		}
+		else {
+			c[k]=b[j];
+			j++;
+			k++;
+		}
+	}
+	while(i<m) {
+		c[k]=a[i];
+		k++;
+		i++;
+	}
+	while(j<n) {
+		c[k]=b[j];
+		k++;
+		j++;
+	}
+	printf("\nArray a : ");
+	for(i=0;i<m;i++)
+	printf("%d ",a[i]);
+	printf("\nArray b : ");
+	for(i=0;i<n;i++)
+	printf("%d ",b[i]);
+	printf("\nArray c : ");
+	for(i=0;i<m+n;i++)
+	printf("%d ",c[i]);
+	getch();
+}
---
+++ sem1/ds/prog02.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+int a[10],front=-1,rear=-1,n;
+void insert() {
+    int x;
+    if((front==0 && rear==n-1)||(front==rear+1)) {
+        printf("Queue is full");
+    }
+    else {
+        printf("Enter the element to insert : ");
+        scanf("%d",&x);
+        if(front==-1 && rear==-1) 
+        front=rear=0;
+        else if(rear==n-1 && front!=0) 
+        rear=0;
+        else 
+        rear=(rear+1)%n;
+        a[rear]=x;
+    }
+}
+void display() {
+    int i;
+    printf("Front = %d\nRear = %d\n",front,rear);
+    if(front==-1) 
+    printf("\nQueue is empty");
+    else if(front<=rear) {
+        for(i=front;i<=rear;i++)
+        printf("%d ",a[i]);
+    }
+    else {
+        for(i=front;i<n;i++)
+        printf("%d ",a[i]);
+        for(i=0;i<=rear;i++)
+        printf("%d",a[i]);
+    }
+}
+void del() {
+    if(front==-1)
+    printf("\nQueue is empty");
+    else {
+        printf("Deleted element : %d",a[front]);
+        if(front==rear)
+        front=rear=-1;
+        else {
+            if(front==n-1)
+            front=0;
+            else
+            front+=1;
+        }
+    }
+}
+void search() {
+    int x,i,j;
+    printf("Enter the element to search : ");
+    scanf("%d",&x);
+    if(front<=rear) {
+        int f1=0;
+        for(i=front;i<=rear;i++) {
+            if(a[i]==x) {
+                printf("Element found at position %d",i);
+                f1=1;
+                break;
+            }
+        }
+        if(f1==0)
+        printf("Element not found");
+    }
+    else {
+        int f=0;
+        for(i=front,j=1;i<n;i++,j++) {
+            if(a[i]==x) {
+                f=1;
+                printf("Element found at position : %d",j);
+                break;
+            }
+        }
+        if(f==0) {
+            int f2=0;
+            for(i=0;i<=rear;i++) {
+                if(a[i]==x) {
+                    printf("Element found at position : %d",i+n-1);
+                    f2=1;
+                    break;
+                }
+            }
+            if(f2==0)
+            printf("Element not found");
+        }
+    }
+}
+void main() {
+    char ch;
+    printf("Enter the size of the queue : ");
+    scanf("%d",&n);
+    while(1) {
+        printf("\n\n1:Insertion");
+        printf("\n2:Deletion");
+        printf("\n3:Search");
+        printf("\n4:Display");
+        printf("\n5:Exit");
+        printf("\nEnter your choice : ");
+        scanf("%s",&ch);
+        switch(ch) {
+            case '1':insert();
+            break;
+            case '2':del();
+            break;
+            case '3':search();
+            break;
+            case '4':display();
+            break;
+            case '5':printf("\nPress any key to exit..");
+            getch();
+            exit(0);
+            break;
+            default:printf("\nInvalid choice");
+        }
+    }
+}
