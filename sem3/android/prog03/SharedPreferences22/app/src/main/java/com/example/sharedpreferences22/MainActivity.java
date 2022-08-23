5447a5ca4fa4fe4a56c657bacbaeedb029e35f4b
NCS <nandusasikumar007@gmail.com>
2022-08-23 11:58:49 +0530

    commit 30

---
+++ sem3/android/prog03/SharedPreferences22/app/src/main/java/com/example/sharedpreferences22/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.sharedpreferences22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText f,l,e,p;
+    Button btn;
+    String f2,l2,e2,p2;
+
+    SharedPreferences sharedpreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        f=(EditText)findViewById(R.id.fname);
+        l=(EditText)findViewById(R.id.lname);
+        e=(EditText)findViewById(R.id.email);
+        p=(EditText)findViewById(R.id.pass);
+        btn=(Button)findViewById(R.id.reg);
+        sharedpreferences = getSharedPreferences("mypref", getApplicationContext().MODE_PRIVATE);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                f2=f.getText().toString();
+                l2=l.getText().toString();
+                e2=e.getText().toString();
+                p2=p.getText().toString();
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                editor.putString("fname", f2);
+                editor.putString("lname", l2);
+                editor.putString("email", e2);
+                editor.putString("password", p2);
+                editor.commit();
+            }
+        });
+    }
+
+}
