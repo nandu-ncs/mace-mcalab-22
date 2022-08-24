c7f6f94a62255f5d1ab6c3325df82ef19353ae4c
Nandu <nandusasikumar007@gmail.com>
2022-08-25 01:56:28 +0530

    commit 32 android prog04 added

---
+++ sem3/android/prog04/ImplicitIntent22/app/src/main/java/com/example/implicitintent22/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.implicitintent22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button browser,playstore,map,phone,message;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        browser = (Button)findViewById(R.id.browser);
+        playstore = (Button)findViewById(R.id.playstore);
+        map = (Button)findViewById(R.id.map);
+        phone = (Button)findViewById(R.id.phone);
+        message = (Button)findViewById(R.id.message);
+        browser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://nandusasikumar.atwebpages.com"));
+                Intent chooser = Intent.createChooser(intent,"Choose your Browser");
+                startActivity(chooser);
+            }
+        });
+        playstore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://market.android.com/details?id=com.instagram.android"));
+                Intent chooser = Intent.createChooser(intent,"Choose Appstore");
+                startActivity(chooser);
+            }
+        });
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:9.967589, 76.422502"));
+                Intent chooser = Intent.createChooser(intent,"Choose Maps");
+                startActivity(chooser);
+            }
+        });
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+"9562396427"));
+                Intent chooser = Intent.createChooser(intent,"Choose Phone");
+                startActivity(chooser);
+            }
+        });
+        message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, "Hi!");
+                intent.setData(Uri.parse("android.resource://" + getPackageName() + "/drawable/" + "pro"));
+                intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("android.resource://" + getPackageName() + "/drawable/" + "pro"));
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.setType("image/jpeg");
+                Intent chooser = Intent.createChooser(intent,"Choose message app");
+                startActivity(chooser);
+            }
+        });
+    }
+}
